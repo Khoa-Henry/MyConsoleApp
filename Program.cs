501ce2b1832dfb77dbe341f3b8aef86f99243905
Henry Nguyen <knguyen@icstars.org>
2025-04-09 18:09:24 -0500

    user adding

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@
             new Teacher(44, "Stacy Read"),
         ];
 
+        AddNewAnimals(zoo);
+
         foreach (Talkable thing in zoo)
         {
             PrintOut(thing);
@@ -34,5 +36,52 @@
     {
         outFile.FileWrite(p.Name + " | " + p.Talk());
         Console.WriteLine(p.Name + " says = " + p.Talk());
+    }
+
+    static void AddNewAnimals(List<Talkable> zoo)
+    {
+        while (true)
+        {
+            Console.WriteLine(
+                "What type of animal do you want to create? (Dog, Cat, Teacher, or type 'exit' to finish):"
+            );
+            string animalType = Console.ReadLine();
+
+            if (animalType.ToLower() == "exit")
+            {
+                break;
+            }
+
+            switch (animalType.ToLower())
+            {
+                case "dog":
+                    Console.WriteLine("Is the dog trained? (true/false):");
+                    bool isTrained = bool.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the dog's name:");
+                    string dogName = Console.ReadLine();
+                    zoo.Add(new Dog(isTrained, dogName));
+                    break;
+
+                case "cat":
+                    Console.WriteLine("Enter the cat's number of lives:");
+                    int lives = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the cat's name:");
+                    string catName = Console.ReadLine();
+                    zoo.Add(new Cat(lives, catName));
+                    break;
+
+                case "teacher":
+                    Console.WriteLine("Enter the teacher's age:");
+                    int age = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the teacher's name:");
+                    string teacherName = Console.ReadLine();
+                    zoo.Add(new Teacher(age, teacherName));
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid animal type. Please try again.");
+                    break;
+            }
+        }
     }
 }
--- obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8379232da33f55edae1b21ed941577ae09f893")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
